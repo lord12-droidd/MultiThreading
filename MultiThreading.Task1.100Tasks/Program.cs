6b216bccf4bc04121a0d19d07422f36a86b8a66f
Dmytro Melnyk2 <dmytro_melnyk2@epam.com>
2022-10-17 23:31:46 +0300

    Add project files.

---
+++ MultiThreading.Task1.100Tasks/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * 1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.
+ * Each Task should iterate from 1 to 1000 and print into the console the following string:
+ * “Task #0 – {iteration number}”.
+ */
+using System;
+using System.Threading.Tasks;
+
+namespace MultiThreading.Task1._100Tasks
+{
+    class Program
+    {
+        const int TaskAmount = 100;
+        const int MaxIterationsCount = 1000;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".Net Mentoring Program. Multi threading V1.");
+            Console.WriteLine("1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.");
+            Console.WriteLine("Each Task should iterate from 1 to 1000 and print into the console the following string:");
+            Console.WriteLine("“Task #0 – {iteration number}”.");
+            Console.WriteLine();
+            
+            HundredTasks();
+
+            Console.ReadLine();
+        }
+
+        static void HundredTasks()
+        {
+            // feel free to add your code here
+
+            Task[] tasks = new Task[TaskAmount];
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = new Task(
+                    () => 
+                    {
+                        for (int j = 0; j < MaxIterationsCount; j++)
+                        {
+                            Output(i, j);
+                        }
+                    });
+                tasks[i].Start();
+                tasks[i].Wait();
+            }
+        }
+
+        static void Output(int taskNumber, int iterationNumber)
+        {
+            Console.WriteLine($"Task #{taskNumber} – {iterationNumber}");
+        }
+    }
+}
---
+++ MultiThreading.Task2.Chaining/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * 2.	Write a program, which creates a chain of four Tasks.
+ * First Task – creates an array of 10 random integer.
+ * Second Task – multiplies this array with another random integer.
+ * Third Task – sorts this array by ascending.
+ * Fourth Task – calculates the average value. All this tasks should print the values to console.
+ */
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace MultiThreading.Task2.Chaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".Net Mentoring Program. MultiThreading V1 ");
+            Console.WriteLine("2.	Write a program, which creates a chain of four Tasks.");
+            Console.WriteLine("First Task – creates an array of 10 random integer.");
+            Console.WriteLine("Second Task – multiplies this array with another random integer.");
+            Console.WriteLine("Third Task – sorts this array by ascending.");
+            Console.WriteLine("Fourth Task – calculates the average value. All this tasks should print the values to console");
+            Console.WriteLine();
+
+            // feel free to add your code
+            Random randomizer = new Random();
+
+            Task<int[]> task1 = new Task<int[]>(() => 
+            {
+                int[] array = new int[10];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = randomizer.Next(-150, 150);
+                }
+                Console.WriteLine($"Result of first task: {String.Join(",", array)}");
+                return array;
+            });
+            task1.Start();
+            Task<int[]> task2 = task1.ContinueWith((taskOneInfo) =>
+            {
+                int multiplier = randomizer.Next(2,5);
+                int[] multipliedArray = taskOneInfo.Result;
+                for (int i = 0; i < multipliedArray.Length; i++)
+                {
+                    multipliedArray[i] = multipliedArray[i] * multiplier;
+                }
+                Console.WriteLine($"Result of second task: {String.Join(",", multipliedArray)}");
+                return multipliedArray;
+            });
+            Task<int[]> task3 = task2.ContinueWith((taskTwoInfo) =>
+            {
+                int[] sortedArray = taskTwoInfo.Result;
+                Array.Sort(sortedArray);
+                Console.WriteLine($"Result of third task: {String.Join(",", sortedArray)}");
+                return sortedArray;
+            });
+            Task task4 = task3.ContinueWith((taskThreeInfo) =>
+            {
+                Console.WriteLine($"Result of fourth task: {String.Join(",", taskThreeInfo.Result.Average())}");
+            });
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
